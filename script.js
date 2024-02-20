4ab40fa3d22765ccd61383a11f105eb6750b7b72
frederickcorredor <144303183+frederickcorredor@users.noreply.github.com>
2024-02-20 16:19:01 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let mostrar1= document.getElementById("mostrar1"),
+    mostrar2= document.getElementById("mostrar2"),
+    mostrar3= document.getElementById("mostrar3"),
+    mostrar4= document.getElementById("mostrar4"),
+    mostrar5= document.getElementById("mostrar5"),
+    mostrar6= document.getElementById("mostrar6");
+
+
+function m1(){
+    mostrar1.style.display="none"
+    mostrar2.style.display="block"
+}
+
+function m2(){
+    mostrar2.style.display="none"
+    mostrar3.style.display="block"
+}
+
+function m3(){
+    mostrar3.style.display="none"
+    mostrar4.style.display="block"
+}
+
+function m4(){
+    mostrar4.style.display="none"
+    mostrar5.style.display="block"
+}
+
+function m5(){
+    mostrar5.style.display="none"
+    mostrar6.style.display="block"
+}
